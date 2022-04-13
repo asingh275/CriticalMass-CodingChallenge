545e856dcc9104bf7ae13745458e81aadc959c41
Arminder Singh <asingh275@mylangara.ca>
2022-04-12 20:09:45 -0700

    categorizing and sorting of menuitems done

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import menu from "./menu";
 
 const menuItems = menu.items;
 
-console.log(menuItems);
+let categories = {};
+
+menuItems.forEach((item) => {
+  let type = item.type;
+  if (!categories[type]) {
+    categories[type] = [item];
+  } else {
+    categories[type].push(item);
+  }
+  categories[type].sort((a, b) => a.menuOrder - b.menuOrder);
+});
