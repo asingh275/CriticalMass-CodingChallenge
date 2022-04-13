1f3b415bfc589235260c1ea00d560221e3bb6442
Arminder Singh <asingh275@mylangara.ca>
2022-04-12 20:22:15 -0700

    Added menuItems div

--- src/index.js
+++ src/index.js
@@ -14,3 +14,36 @@ menuItems.forEach((item) => {
   }
   categories[type].sort((a, b) => a.menuOrder - b.menuOrder);
 });
+
+const makeItemElement = (item) => {
+  const itemDiv = document.createElement("div");
+
+  const nameElem = document.createElement("h3");
+  const descriptionElem = document.createElement("p");
+  const priceElem = document.createElement("p");
+
+  const name = document.createTextNode(item.name);
+  const description = document.createTextNode(item.description);
+  const price = document.createTextNode(item.price);
+
+  nameElem.appendChild(name);
+  descriptionElem.appendChild(description);
+  priceElem.appendChild(price);
+
+  itemDiv.innerHTML +=
+    nameElem.outerHTML + descriptionElem.outerHTML + priceElem.outerHTML;
+
+  if (item.spicy) {
+    itemDiv.classList.add("spicy");
+  }
+
+  return itemDiv;
+};
+
+Object.keys(categories).forEach((type) => {
+  const parent = document.getElementById(type);
+
+  categories[type].forEach((item) => {
+    parent.appendChild(makeItemElement(item));
+  });
+});
